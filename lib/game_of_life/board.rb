feebe845171b987b9a09ac8de677af0de0b7d2fb
MaxAlmeida <llewxam100@hotmail.com>
2019-12-31 17:11:43 -0300

    Make modifications in board

--- lib/game_of_life/board.rb
+++ lib/game_of_life/board.rb
@@ -1,42 +1,42 @@
-require 'game_of_life/cell'
+require_relative 'cell'
 
 module GameOfLife
   class Board
-    attr_reader :cells
-  
-    def initialize cell_quantity
-      set_board_limit(cell_quantity)
+    attr_reader :cells, :board_limit
 
-      cells = (0..cell_quantity - 1).to_a.map do |x|
-        (0..cell_quantity - 1).to_a.map do |y|
-          Cell.new(x, y)
+    def initialize cell_quantity
+      
+      set_board_limit(cell_quantity)
+      cells = (0..cell_quantity - 1).to_a.map do |y|
+        (0..cell_quantity - 1).to_a.map do |x|
+          Cell.new(x, y,self)
         end
       end
+      @cells = cells
+    end
 
-      @cells = cells.flatten
+    def setup_seed(seed)
+        seed.each do |seed|
+          puts get_cell(seed[0],seed[1])
+          get_cell(seed[0],seed[1]).revive
+        end
+    end
+
+    def display
+      system "clear"
+      puts @cells.map {|row| row.map {|cell| cell.status == 1 ? '#' : '.'}.join(' ')}
     end
 
     def get_cell x, y
-      @cells.find { |cell| cell.x == x && cell.y == y }
-    end
-
-    def decide_cell_status cell
-      case count_neighbors(cell)
-      when 0..1
-        GameOfLife::Cell::DEAD
-      when 2..3
-        GameOfLife::Cell::ALIVE
-      when 4..8
-        GameOfLife::Cell::DEAD
-      end
+      @cells.flatten.find { |cell| cell.x == x && cell.y == y }
     end
 
     def next_generation_status 
-      next_status_map = @cells.map do |cell|
+      next_status_map = @cells.flatten.map do |cell|
         {
           x: cell.x,
           y: cell.y,
-          next: decide_cell_status(cell)
+          next: cell.decide_status
         }
       end
     end
@@ -45,35 +45,11 @@ module GameOfLife
       next_generation_status.map do |status|
         get_cell(status[:x], status[:y]).set(status[:next])
       end
-    end
-
-    def neighbors
-      [
-        { x: - 1, y: - 1 },
-        { x: + 0, y: - 1 },
-        { x: + 1, y: - 1 },
-        { x: - 1, y: + 0 },
-        { x: + 1, y: + 0 },
-        { x: - 1, y: + 1 },
-        { x: + 0, y: + 1 },
-        { x: + 1, y: + 1 },
-      ]
-    end
-
-    def count_neighbors cell
-      neighbors
-      .map { |position| { x: position[:x] + cell.x, y: position[:y] + cell.y } }
-      .reject { |position| invalid_position?(position) }
-      .map { |position| get_cell(position[:x], position[:y]).status }
-      .sum
-    end
-
-    def invalid_position? position
-      position[:x] < 0 || position[:x] > @board_limit || position[:y] < 0 || position[:y] > @board_limit
+      display()
     end
 
     private def set_board_limit cell_quantity
-      @board_limit = cell_quantity - 1
+      @board_limit = cell_quantity-1
     end
   end
 end
