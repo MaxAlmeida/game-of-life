10ee62afaefd84bafa6402ebf9f5cadfaf5b1c82
MaxAlmeida <llewxam100@hotmail.com>
2020-01-01 21:26:17 -0300

    refactoring board tests

--- spec/game_of_life/board_spec.rb
+++ spec/game_of_life/board_spec.rb
@@ -2,54 +2,86 @@ require 'game_of_life/board'
 
 RSpec.describe GameOfLife::Board do
   let(:board) { described_class.new(3) }
+  let(:seed) { [[1,0]]}
+  let(:seed_off_limits) {[[10,10]]}
 
-  describe 'can create a new square board with n x n cells' do
-    it { expect(board.cells.count).to eq(9) }
-  end
-
-  it '#get_cell returns a based on x and y' do
-    expect(board.get_cell(1,1).class).to eq(GameOfLife::Cell)
-  end
-
-  it '#count_neighbors' do
-    board.get_cell(0,1).revive
-    board.get_cell(1,1).revive
-    board.get_cell(2,1).revive
-    expect(board.count_neighbors(board.get_cell(1,1))).to eq(2)
-  end
-
-  describe '#next_generation keeps alive cells with two neighbors' do
-    before do
-      board.get_cell(0,1).revive
-      board.get_cell(1,1).revive
-      board.get_cell(2,1).revive
-      board.next_generation
+  context "when game of life it started initialize board " do
+    describe "#cells" do
+      it 'create square board with nxn cells' do
+        expect(board.cells.flatten.count).to eq(9)
+      end
     end
 
-    it { expect(board.get_cell(0,1).status).to eq(GameOfLife::Cell::DEAD) }
-    it { expect(board.get_cell(1,1).status).to eq(GameOfLife::Cell::ALIVE) }
+    describe "#get_cell" do
+      it 'returns a based on x and y' do
+        expect(board.get_cell(1,1).class).to eq(GameOfLife::Cell)
+      end
+    end
+
+    describe "#setup_seed" do
+      it "Revive cells defined in seed" do
+        board.setup_seed(seed)
+        expect(board.get_cell(1,0).status).to eq(GameOfLife::Cell::ALIVE)
+      end
+
+      it "Raise error when seed off limits" do
+        expect { board.setup_seed(seed_off_limits) }.to raise_exception(SystemExit)
+      end
+    end
   end
 
-  it '#next_generation cell dies if has more than neighbors' do
-    board.get_cell(0,0).revive
-    board.get_cell(1,0).revive
-    board.get_cell(2,1).revive
-    board.get_cell(1,1).revive
-    board.get_cell(0,1).revive
-  
-    expect { board.next_generation }.to change { board.get_cell(1,1).status }.to(GameOfLife::Cell::DEAD)  
-  end
+  context "when board receive seeds with cells alive" do
+    describe '#next_generation' do
+      it 'Living cell without neighbor will dies' do
+        board.get_cell(0,0).revive
+        expect { board.next_generation }.to change { board.get_cell(0,0).status }.to(GameOfLife::Cell::DEAD)
+      end
 
-  it '#next_generation cell dies if is alone' do
-    board.get_cell(0,0).revive
-  
-    expect { board.next_generation }.to change { board.get_cell(0,0).status }.to(GameOfLife::Cell::DEAD)  
-  end
+      it 'Living cell with 1 neighbor will dies' do
+        board.get_cell(0,0).revive
+        board.get_cell(0,1).revive
+        expect { board.next_generation }.to change { board.get_cell(0,0).status }.to(GameOfLife::Cell::DEAD)
+      end
 
-  it '#next_generation cell dies if has one neighbor' do
-    board.get_cell(0,0).revive
-    board.get_cell(0,1).revive
-  
-    expect { board.next_generation }.to change { board.get_cell(0,0).status }.to(GameOfLife::Cell::DEAD)  
+      it 'Living cell with 2 neighbors remains alive ' do
+        board.get_cell(0,1).revive
+        board.get_cell(1,1).revive
+        board.get_cell(2,1).revive
+        board.next_generation
+        expect(board.get_cell(1,1).status).to eq(GameOfLife::Cell::ALIVE)
+      end
+
+      it 'Dead cell with 2 neighbors remains dead' do
+        board.get_cell(0,1).revive
+        board.get_cell(2,1).revive
+        board.next_generation
+        expect(board.get_cell(1,1).status).to eq(GameOfLife::Cell::DEAD)
+      end
+
+      it "Living cell with 3 neighbors remains alive" do
+        board.get_cell(0,0).revive
+        board.get_cell(1,0).revive
+        board.get_cell(1,1).revive
+        board.get_cell(0,1).revive
+        board.next_generation
+        expect(board.get_cell(1,1).status).to eq(GameOfLife::Cell::ALIVE)
+      end
+
+      it "Dead cell with 3 neighbors will stay alive" do
+        board.get_cell(0,0).revive
+        board.get_cell(1,0).revive
+        board.get_cell(0,1).revive
+        expect { board.next_generation }.to change { board.get_cell(1,1).status }.to(GameOfLife::Cell::ALIVE)
+      end
+
+      it "Living cell with more than 3 neighbors will dies " do
+        board.get_cell(0,0).revive
+        board.get_cell(1,0).revive
+        board.get_cell(2,1).revive
+        board.get_cell(1,1).revive
+        board.get_cell(0,1).revive
+        expect { board.next_generation }.to change { board.get_cell(1,1).status }.to(GameOfLife::Cell::DEAD)
+      end
+    end
   end
 end
